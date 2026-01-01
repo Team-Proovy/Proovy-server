baf0c8b9c3272e8b8e1a5074d7a9885ad33553f8
gaeunee2 <gaeunee2>
2026-01-02 04:23:24 +0900

    chore: 프로젝트 초기 설정

---
+++ src/test/java/com/proovy/api/ProovyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proovy.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProovyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
