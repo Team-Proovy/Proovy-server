a903e1d67386e92cb9a43423a439352297c69199
DESKTOP-TROTBC9\samso <samsockbee@gmail.com>
2026-01-13 16:16:24 +0900

    feat: Auth Entity 추가

---
+++ src/main/java/com/proovy/domain/auth/entity/AuthAccount.java
@@ -0,0 +1,55 @@
+package com.proovy.domain.auth.entity;
+
+import com.proovy.domain.user.entity.AuthProvider;
+import com.proovy.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(
+        name = "auth_accounts",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "uk_auth_accounts_provider_provider_user_id",
+                        columnNames = {"provider", "provider_user_id"}
+                )
+        }
+)
+public class AuthAccount {
+
+    @Getter
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "auth_account_id")
+    private Long authAccountId;
+
+    @Getter
+    @Enumerated(EnumType.STRING)
+    @Column(name = "provider", length = 10, nullable = false)
+    private AuthProvider provider;
+
+    @Getter
+    @Column(name = "provider_user_id", length = 255, nullable = false)
+    private String providerUserId;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false,
+            foreignKey = @ForeignKey(name = "fk_users_to_auth_accounts_1"))
+    private User user;
+
+    protected AuthAccount() {
+    }
+
+    public AuthAccount(User user, AuthProvider provider, String providerUserId) {
+        this.user = user;
+        this.provider = provider;
+        this.providerUserId = providerUserId;
+        this.createdAt = LocalDateTime.now();
+    }
+
+}
