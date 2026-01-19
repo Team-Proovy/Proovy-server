dab3c0458f5bd19f8cb343b1318394cddb1dd95b
gaeunee2 <gaeunee2>
2026-01-19 13:33:34 +0900

    chore: add application entrypoint

---
+++ src/main/java/com/proovy/ProovyApiApplication.java
@@ -0,0 +1,13 @@
+package com.proovy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProovyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProovyApiApplication.class, args);
+	}
+
+}
