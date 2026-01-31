54e81c462c167adfa2830ce3ee39232eed5eff46
gaeunee2 <gaeunee2>
2026-01-31 15:56:58 +0900

    feat: AWS RDS, Redis 연동 및 EC2 서버 배포

--- src/main/java/com/proovy/global/config/SwaggerConfig.java
+++ src/main/java/com/proovy/global/config/SwaggerConfig.java
@@ -33,12 +33,7 @@ public class SwaggerConfig {
                         .description("Proovy 백엔드 API 명세서")
                         .version("v1.0.0"))
                 .servers(List.of(
-                        new Server()
-                                .url("http://localhost:8080")
-                                .description("로컬 개발 서버"),
-                        new Server()
-                                .url("https://api.proovy.com")
-                                .description("프로덕션 서버")
+                        new Server().url("/")
                 ))
                 .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
                 .components(new Components()
